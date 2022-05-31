4f375f59b131afe47687c4f692f8da53991e1225
Ileis <gabrielileis07@gmail.com>
2022-05-31 00:03:34 -0300

    frequencia.hs comitado

--- Recurcao/frequencia.hs
+++ Recurcao/frequencia.hs
@@ -11,3 +11,14 @@ frequencia2 x xs = length [y | y <- xs, y == x]
 frequencia3 x [] = 0
 frequencia3 x (y:ys) | x == y = 1 + frequencia3 x ys
                      | otherwise = frequencia3 x ys
+
+-- opcao 4
+
+frequencia4 x xs = foldl (\acc y -> if y == x then acc + 1 else acc) 0 xs
+
+-- frequencia4 1 [] == 0
+-- frequencia4 4 [4] == 1
+-- frequencia4 4 [5] == 0
+-- frequencia4 4 [4,4] == 2
+-- frequencia4 2 [4,4] == 0
+-- frequencia4 5 [4,5,2,1,5,5,9] == 3
