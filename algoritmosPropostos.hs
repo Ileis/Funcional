45ceb9455e94da08415929449f53eedee1ef46b1
Ileis <gabrielileis07@gmail.com>
2022-06-07 17:36:18 -0300

    implementacao foldl e foldr

--- algoritmosPropostos.hs
+++ algoritmosPropostos.hs
@@ -33,6 +33,7 @@ list1 = [1..10]
 list2 = [x * (-1) | x <- list1]
 
 -- MAP
+
 map' :: (a -> b) -> [a] -> [b]
 map' f [] = []
 map' f (x:xs) = f x : map' f xs
@@ -49,3 +50,35 @@ filter' f (x:xs) | f x       = x : filter' f xs
 
 filter1 :: (a -> Bool) -> [a] -> [a]
 filter1 f xs = [x | x <- xs, f x]
+
+-- FOLDL
+foldl' f acc [] = acc
+foldl' f acc (x:xs) = foldl' f (f acc x) xs
+
+-- FOLDR
+
+foldr' f acc [] = acc
+foldr' f acc (x:xs) = f x (foldr' f acc xs)
+
+-- Detalhes da execucao da funcao foldr'.
+-- Pontos a serem considerados:
+--  - Pilha formada
+--  - Recurcao de calda
+
+-- foldr' (+) 0 [1,2,3,4,5] = (+) 1 (foldr' (+) 0 [2,3,4,5])
+-- foldr' (+) 0   [2,3,4,5] = (+) 2 (foldr' (+) 0 [3,4,5])
+-- foldr' (+) 0     [3,4,5] = (+) 3 (foldr' (+) 0 [4,5])
+-- foldr' (+) 0       [4,5] = (+) 4 (foldr' (+) 0 [5])
+-- foldr' (+) 0         [5] = (+) 5 (foldr' (+) 0 [])
+-- foldr' (+) 0          [] = 0
+-- foldr' (+) 0         [5] = (+) 5 0 = 5
+-- foldr' (+) 0       [4,5] = (+) 4 5 = 9
+-- foldr' (+) 0     [3,4,5] = (+) 3 9 = 12
+-- foldr' (+) 0   [2,3,4,5] = (+) 2 12 = 14
+-- foldr' (+) 0 [1,2,3,4,5] = (+) 1 14 = 15
+
+
+
+
+
+elem' x = foldl' (\acc y -> if y == x then True else acc) False
