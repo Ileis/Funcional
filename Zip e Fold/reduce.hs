63463e53dce8ee18c67e231c39db89807220ec25
Ileis <gabrielileis07@gmail.com>
2022-07-14 14:52:45 -0300

    reduce.hs comitado

---
+++ Zip e Fold/reduce.hs
@@ -0,0 +1,6 @@
+import Data.Tuple (swap)
+reduce (a, b) = reduce' (a, b) [2..b]
+
+reduce' (a, b) [] = (a, b)
+reduce' (a, b) (x:xs) = if ((a `mod` x) == 0 && (b `mod` x) == 0) then reduce' (divT (a, b) x) [2..b `div` x] else reduce' (a, b) xs
+divT (a, b) x = (a `div` x, b `div` x)
